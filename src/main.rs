9c5a5efee99a9ee6262f638a840e51c750fa719f
twentyone212121 <03denys.k@gmail.com>
2024-12-15 15:00:30 +0900

    feat(index): added observer to detect and add new files to the index

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-mod thread_pool;
 mod inverted_index;
+mod thread_pool;
 
-use thread_pool::ThreadPool;
-use inverted_index::{InvertedIndex, Document};
+use inverted_index::{Document, InvertedIndex};
 use std::{
-    fs, io::{self, prelude::*, BufReader}, net::{TcpListener, TcpStream}, path::{Path, PathBuf}, sync::Arc
+    collections::HashMap,
+    fs,
+    io::{self, prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    path::{Path, PathBuf},
+    sync::Arc,
+    thread,
+    time::{Duration, SystemTime},
 };
+use thread_pool::ThreadPool;
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,7 +20,10 @@ fn main() -> io::Result<()> {
     let index = Arc::new(InvertedIndex::new());
     let corpus_dir = "/Users/deniskyslytsyn/Documents/КПІшка))/7 семестр/ПО курс/aclImdb/train/pos";
 
-    for path in txt_files_in_dir(corpus_dir)? {
+    let corpus = txt_files_in_dir(corpus_dir)?;
+
+    // Index the corpus
+    for path in corpus.clone() {
         let index = Arc::clone(&index);
         pool.execute(move || {
             if let Err(e) = add_file_to_index(path.as_path(), &index) {
@@ -22,9 +32,32 @@ fn main() -> io::Result<()> {
         })
     }
     pool.join();
-    println!("Indexing complete. Total documents: {}", index.document_count());
+    println!(
+        "Indexing complete. Total documents: {}",
+        index.document_count()
+    );
     println!("Unique terms: {}", index.term_count());
 
+    // Set up the observer
+    {
+        let index = Arc::clone(&index);
+        pool.execute(move || {
+            watch_directory(
+                corpus_dir,
+                corpus,
+                Duration::from_secs(1),
+                |path| {
+                    println!("File {} detected", path.to_string_lossy());
+                    if let Err(e) = add_file_to_index(path, &index) {
+                        eprintln!("Error processing file {}: {}", path.to_string_lossy(), e);
+                    }
+                },
+                |_| return,
+            );
+        });
+    }
+
+    // Listen for connections
     for stream in listener.incoming().take(10) {
         let stream = stream.unwrap();
 
@@ -39,13 +72,57 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn watch_directory<F, G>(
+    dir_path: &str,
+    present_files: Vec<PathBuf>,
+    interval: Duration,
+    on_new_file: F,
+    on_modified_file: G,
+) where
+    F: Fn(&Path),
+    G: Fn(&Path),
+{
+    let mut present_files = present_files
+        .into_iter()
+        .map(|path_buf| {
+            let modified = path_buf.metadata().map_or(SystemTime::now(), |meta| {
+                meta.modified().unwrap_or(SystemTime::now())
+            });
+            (path_buf, modified)
+        })
+        .collect::<HashMap<PathBuf, SystemTime>>();
+
+    loop {
+        if let Ok(new_paths) = txt_files_in_dir(dir_path) {
+            for path in new_paths {
+                let modified = path.metadata().map_or(SystemTime::now(), |meta| {
+                    meta.modified().unwrap_or(SystemTime::now())
+                });
+                match present_files.get(&path) {
+                    Some(prev_modified) => {
+                        if *prev_modified < modified {
+                            // File was modified
+                            on_modified_file(&path);
+                        }
+                    }
+                    None => {
+                        // File was added
+                        on_new_file(&path);
+                    }
+                }
+                present_files.insert(path, modified);
+            }
+        }
+        thread::sleep(interval);
+    }
+}
+
 fn txt_files_in_dir(dir_path: &str) -> io::Result<Vec<PathBuf>> {
     Ok(fs::read_dir(dir_path)?
         .filter_map(|entry| entry.ok())
         .map(|entry| entry.path())
         .filter(|path| path.is_file() && path.extension().map_or(false, |ext| ext == "txt"))
-        .collect::<Vec<_>>()
-    )
+        .collect::<Vec<_>>())
 }
 
 fn add_file_to_index(path: &Path, index: &InvertedIndex) -> io::Result<()> {
@@ -92,8 +169,7 @@ fn handle_connection(mut stream: TcpStream, index: &InvertedIndex) {
     };
 
     let length = contents.len();
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
 
